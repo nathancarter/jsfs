ca748a6e69ae524df92b9416a3014bf3f592882b
Nathan Carter <ncarter@bentley.edu>
2014-11-02 23:07:06 -0500

    Attempting to set up Travis-CI integration

---
+++ tests/phantom-runner.js
@@ -0,0 +1,46 @@
+
+//
+// Credit for this goes to Lorenzo Planas, who published it here:
+// https://github.com/qindio/headless-jasmine-sample
+// He released it under the MIT license.  Thanks, Lorenzo!
+//
+
+var system = require('system');
+
+if (system.args.length !== 2) {
+    console.log('Usage: run-jasmine.js URL');
+    phantom.exit(1);
+}
+
+// Check for console message indicating jasmine is finished running
+var doneRegEx = /^\d+ specs, (\d+) failure/;
+var noReallyDoneRegEx = /^Finished in \d[\d\.]* second/;
+var rc;
+
+var page = require('webpage').create();
+
+// Route "console.log()" calls from within the Page context
+// to the main Phantom context (i.e. current "this")
+
+page.onConsoleMessage = function (msg) {
+    system.stdout.write(msg);
+    var match = doneRegEx.exec(msg);
+    if (match) {
+        rc = match[1]==="0" ? 0 : 1;
+        return;
+    }
+    match = noReallyDoneRegEx.exec(msg);
+    if (match) {
+        system.stdout.writeLine("");
+        setTimeout(function(){phantom.exit(rc);},0);
+    }
+};
+
+system.stdout.writeLine("");
+page.open(system.args[1], function(status){
+    if (status !== "success") {
+        console.log("Couldn't load the page");
+    }
+    system.stdout.writeLine("");
+});
+
---
+++ tests/phantom-setup.js
@@ -0,0 +1,18 @@
+
+//
+// Credit for this goes to Lorenzo Planas, who published it here:
+// https://github.com/qindio/headless-jasmine-sample
+// He released it under the MIT license.  Thanks, Lorenzo!
+//
+
+if (navigator.userAgent.indexOf("PhantomJS") > 0) {
+    var consoleReporter = new jasmineRequire.ConsoleReporter()({
+        showColors: true,
+        timer: new jasmine.Timer,
+        print: function() {
+          console.log.apply(console, arguments)
+        }
+    });
+    jasmine.getEnv().addReporter(consoleReporter);
+}
+
